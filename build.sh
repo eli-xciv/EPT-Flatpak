ba29774a0bb281f81a7fe6dc1515a1ec3531c2ed
Grady Rein <reingrady@gmail.com>
2023-12-12 23:27:41 -0500

    fixed start on wayland and added pulse audio

--- build.sh
+++ build.sh
@@ -7,10 +7,13 @@ mkdir -p dist/fp-build-dir
 cd dist
 
 # echo retrieving EPT snap image
-# wget https://api.snapcraft.io/api/v1/snaps/download/6C4nJpuGFmcnRFZTx40PpLgTEr2DqYxZ_2.snap
+wget https://api.snapcraft.io/api/v1/snaps/download/6C4nJpuGFmcnRFZTx40PpLgTEr2DqYxZ_2.snap
 
 echo "exploding ept snap"
 unsquashfs -q -f -d "squashfs/ept" "6C4nJpuGFmcnRFZTx40PpLgTEr2DqYxZ_2.snap"
 
 # Run the build
 flatpak-builder --user --install --force-clean dist/fp-build-dir io.github.elixciv.EntropyPianoTuner.yaml
+
+
+
